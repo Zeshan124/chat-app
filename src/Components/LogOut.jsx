c9e036753c606c9e8127d42b0348a6e7b424e777
Zeshan124 <raeeszeeshan4@gmail.com>
2024-03-10 21:34:42 +0500

    first commit

---
+++ src/Components/LogOut.jsx
@@ -0,0 +1,20 @@
+import React from 'react';
+import { auth } from '../firebase';
+
+const style = {
+  button: `bg-gray-200 px-4 py-2 hover:bg-gray-100`,
+};
+
+const LogOut = () => {
+  const signOut = () => {
+    auth.signOut(); // Use auth.signOut() instead of signOut(auth)
+  };
+
+  return (
+    <button onClick={signOut} className={style.button}>
+      Logout
+    </button>
+  );
+};
+
+export default LogOut;
